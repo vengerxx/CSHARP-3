4e3c24c6196061ceeefe6ef7d6bc2dc6b860025d
Dimasik <pak.dima.2006@mail.ru>
2023-09-18 15:43:11 +0600

    DZ

---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿﻿/* 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+System.Console.WriteLine("Введите М: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int SumNuturalNum(int min, int max)
+{   if (min == max) return min;
+    else
+    {
+       return min+SumNuturalNum(min+1,max);
+    }    
+}   ;
+System.Console.WriteLine($"Сумма чисел в промежутке от {m} до {n} равна: {SumNuturalNum(m,n)}");
